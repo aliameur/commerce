407d042af042cb4516e6f0daaa1f99108077decc
Ali Ameur <aliameur169@gmail.com>
2024-04-26 06:40:28 +0100

    fix: imports with new src directory

--- src/components/product/gallery.tsx
+++ src/components/product/gallery.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
-import { GridTileImage } from 'components/grid/tile';
-import { createUrl } from 'lib/utils';
+import { GridTileImage } from '@/components/grid/tile';
+import { createUrl } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
